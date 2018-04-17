96e97cd4e9c01aadc857000fc3af1e057246c543
gstok <3081716662@qq.com>
2018-04-17 20:42:00 +0800

    一个稍微复杂一些的数据绑定

--- src/index.js
+++ src/index.js
@@ -2,53 +2,50 @@
 
 import * as d3 from 'd3';
 
-let p = d3.select("#d3-area").selectAll("p").text("你好，世界");
-p.style("color", "#549233");
-p.style("font-size", "16px");
+let d3Area = d3.select("#d3-area");
+let li = d3Area.selectAll("ul > li");
+let dataSet = [
+    {
+        name: "顾世豪",
+        sex: "男",
+        age: 24,
+        address: "浙江省杭州市"
+    },
+    {
+        name: "习近平",
+        sex: "男",
+        age: "72",
+        address: "陕西某一个窑洞里"
+    },
+    {
+        name: "彭丽媛",
+        sex: "女",
+        age: "61",
+        address: "我也不知道是哪里人"
+    }
+];
 
-let dataSet = Array(p.size()).fill(0).map((item, index) => index + 1).reverse();
-dataSet.push(33);
-dataSet.push(67);
+li.data(dataSet);
 
-//p.datum("无序列表项目");
-let update = p.data(dataSet);
+li
+.append("a").attr("href", "#").text((d, i) => i.toString());
+li
+.append("input").attr("type", "text").property("value", (d, i) => {
+    return d.name;
+})
+li
+.append("span").text("男");
+li
+.append("input").attr("type", "radio").property("checked", d => d.sex == "男");
+li
+.append("span").text("女");
+li
+.append("input").attr("type", "radio").property("checked", d => d.sex == "女");
+li
+.append("span").text("年龄");
+li
+.append("input").attr("type", "range").attr("min", 0).attr("max", 100).property("value", d => d.age);
+li
+.append("textarea").text(d => d.address);
 
-console.log(update);
-console.log(update.enter());
-console.log(update.exit());
-
-p.text((d, i) => {
-    return (d + "  " + (i + 1)).toString();
-});
-
-p.append("span").text((d, i) => {
-    return d.toString();
-});
-
-
-// console.log(li.empty());
-// console.log(li.node());
-// console.log(li.size());
-
-// let mySvg = d3.select("body").append("svg")
-//     .attr("width", 640)
-//     .attr("height", 480);
-// mySvg.classed("mysvg", true);
-
-// console.log(mySvg.classed("mysvg"));
-
-// mySvg.append("circle")
-//     .attr("cx", 30)
-//     .attr("cy", 30)
-//     .attr("r", 20)
-//     .attr("fill", "#93e255");
-
-// let body = d3.select("body");
-// let input = body.append("input");
-// input.attr("type", "text");
-// input.attr("value", "你好，世界");
-// input.property("value", "你不好，世界");
-
-// let span = body.insert("span", "input").text("这里是span");
-// body.insert("textarea", "span").text("这里是textarea");
-// span.remove();
+console.log(li.size());
