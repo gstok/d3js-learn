5205862decaeefe88737e9c847b3410b0d3ee578
gstok <3081716662@qq.com>
2018-04-17 17:48:09 +0800

    子元素继承数据并绑定

--- src/index.js
+++ src/index.js
@@ -2,33 +2,43 @@
 
 import * as d3 from 'd3';
 
-let li = d3.select("ul").selectAll("li").text("你好，世界");
-li.style("color", "red");
-li.style("font-size", "12px");
+let p = d3.select("#d3-area").selectAll("p").text("你好，世界");
+p.style("color", "#549233");
+p.style("font-size", "16px");
 
-console.log(li.empty());
-console.log(li.node());
-console.log(li.size());
+p.datum("无序列表项目");
+p.text((d, i) => {
+    return (d + "  " + (i + 1)).toString();
+});
 
-let mySvg = d3.select("body").append("svg")
-    .attr("width", 640)
-    .attr("height", 480);
-mySvg.classed("mysvg", true);
+p.append("span").text((d, i) => {
+    return d.toString();
+});
 
-console.log(mySvg.classed("mysvg"));
 
-mySvg.append("circle")
-    .attr("cx", 30)
-    .attr("cy", 30)
-    .attr("r", 20)
-    .attr("fill", "#93e255");
+// console.log(li.empty());
+// console.log(li.node());
+// console.log(li.size());
 
-let body = d3.select("body");
-let input = body.append("input");
-input.attr("type", "text");
-input.attr("value", "你好，世界");
-input.property("value", "你不好，世界");
+// let mySvg = d3.select("body").append("svg")
+//     .attr("width", 640)
+//     .attr("height", 480);
+// mySvg.classed("mysvg", true);
 
-let span = body.insert("span", "input").text("这里是span");
-body.insert("textarea", "span").text("这里是textarea");
-span.remove();
+// console.log(mySvg.classed("mysvg"));
+
+// mySvg.append("circle")
+//     .attr("cx", 30)
+//     .attr("cy", 30)
+//     .attr("r", 20)
+//     .attr("fill", "#93e255");
+
+// let body = d3.select("body");
+// let input = body.append("input");
+// input.attr("type", "text");
+// input.attr("value", "你好，世界");
+// input.property("value", "你不好，世界");
+
+// let span = body.insert("span", "input").text("这里是span");
+// body.insert("textarea", "span").text("这里是textarea");
+// span.remove();
