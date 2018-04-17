ee4126bb4051ac2a4a7bfc69f160eb7f095b1c14
gstok <3081716662@qq.com>
2018-04-17 21:00:55 +0800

    键函数的意义：第一次的时候确定键顺序，之后的绑定只不过是为键重新定位索引

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ let d3Area = d3.select("#d3-area");
 let li = d3Area.selectAll("ul > li");
 let dataSet = [
     {
+        id: 72,
         name: "顾世豪",
         sex: "男",
         age: 24,
         address: "浙江省杭州市"
     },
     {
+        id: 36,
         name: "习近平",
         sex: "男",
         age: "72",
         address: "陕西某一个窑洞里"
     },
     {
+        id: 10,
         name: "彭丽媛",
         sex: "女",
         age: "61",
@@ -25,27 +28,81 @@ let dataSet = [
     }
 ];
 
-li.data(dataSet);
 
-li
-.append("a").attr("href", "#").text((d, i) => i.toString());
-li
+function bindData (dataSet) {
+    li.data(dataSet);
+    let indiv = li.append("div");
+    indiv
+    .append("a").attr("href", "#").text(d => d.id);
+    indiv
+    .append("input").attr("type", "text").property("value", (d, i) => {
+        return d.name;
+    })
+    indiv
+    .append("span").text("男");
+    indiv
+    .append("input").attr("type", "radio").property("checked", d => d.sex == "男");
+    indiv
+    .append("span").text("女");
+    indiv
+    .append("input").attr("type", "radio").property("checked", d => d.sex == "女");
+    indiv
+    .append("span").text("年龄");
+    indiv
+    .append("input").attr("type", "range").attr("min", 0).attr("max", 100).property("value", d => d.age);
+    indiv
+    .append("textarea").text(d => d.address);
+}
+
+bindData(dataSet);
+li.select("div").remove();
+
+dataSet = [
+    {
+        id: 10,
+        name: "顾世豪",
+        sex: "男",
+        age: 24,
+        address: "浙江省杭州市"
+    },
+    {
+        id: 36,
+        name: "习近平",
+        sex: "男",
+        age: "72",
+        address: "陕西某一个窑洞里"
+    },
+    {
+        id: 72,
+        name: "彭丽媛",
+        sex: "女",
+        age: "61",
+        address: "我也不知道是哪里人"
+    }
+];
+
+li.data(dataSet, d => d.id);
+let indiv = li.append("div");
+indiv
+.append("a").attr("href", "#").text(d => d.id);
+indiv
 .append("input").attr("type", "text").property("value", (d, i) => {
     return d.name;
 })
-li
+indiv
 .append("span").text("男");
-li
+indiv
 .append("input").attr("type", "radio").property("checked", d => d.sex == "男");
-li
+indiv
 .append("span").text("女");
-li
+indiv
 .append("input").attr("type", "radio").property("checked", d => d.sex == "女");
-li
+indiv
 .append("span").text("年龄");
-li
+indiv
 .append("input").attr("type", "range").attr("min", 0).attr("max", 100).property("value", d => d.age);
-li
+indiv
 .append("textarea").text(d => d.address);
+
 
 console.log(li.size());
