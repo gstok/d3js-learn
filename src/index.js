562dd3391ede60be2d405395c1a9b34f94b43f6f
gstok <3081716662@qq.com>
2018-04-21 11:06:19 +0800

    量子比例尺线性比例尺结合使用

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 
 import * as d3 from "d3";
 
-let pow = d3.scalePow();
-pow.exponent(3);
 
-pow.domain([0, 5]);
-pow.range([0, 1000]);
+let linear = d3.scaleLinear();
+linear.domain([0, 100]);
+linear.range([10, 50]);
 
-console.log(pow(1.5));
+
+let quantize = d3.scaleQuantize();
+quantize.domain([0, 100]);
+quantize.range(["#333", "#555", "#777", "#999", "#bbb", "#ddd"].reverse());
+
+let dataSet = [5, 10, 30, 40, 50, 60, 70, 80];
+
+let mySvg = d3.select("svg");
+
+let update = mySvg.selectAll("circle").data(dataSet);
+
+let enter = update.enter();
+
+enter.append("circle")
+    .attr("cx", (d, i) => i * 70 + 50)
+    .attr("cy", 300)
+    .attr("r", d => linear(d))
+    .attr("fill", d => quantize(d));
+
