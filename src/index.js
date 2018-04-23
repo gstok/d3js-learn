74c7d2342467bb50b36c741e01aaf22c57836412
gstok <3081716662@qq.com>
2018-04-23 17:15:59 +0800

    线性比例尺坐标轴

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 
 import * as d3 from "d3";
 
-let threshold = d3.scaleThreshold();
-threshold.domain([10, 20, 30]);
-threshold.range(["一", "二", "三", "四"]);
-console.log(threshold(30));
+let linear = d3.scaleLinear();
+linear.domain([0, 100]);
+linear.range([0, 200]);
+
+let axis = d3.axisBottom(linear);
+axis.ticks(5);
+// axis.tickValues([0, 1, 10, 100]);
+axis.tickSizeInner(2);
+axis.tickSizeOuter(20);
+axis.tickFormat(d3.format("0.1f"));
+
+
+let svg = d3.select("svg");
+let gAxis = svg.append("g");
+gAxis.attr("transform", "translate(80, 80)"); 
+
+axis(gAxis);
