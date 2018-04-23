9c8ddad79b69a601b3ff076e13d01a9a81c6d4b1
gstok <3081716662@qq.com>
2018-04-23 09:16:27 +0800

    序数比例尺band

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
 
 import * as d3 from "d3";
 
-//普通的序数比例尺
-let ordinal1 = d3.scaleOrdinal();
-ordinal1.domain([1, 2, 3, 4, 5]);
-ordinal1.range(["一", "二", "三", "四", "五"]);
-console.log(ordinal1(3));
+let scaleBand = d3.scaleBand();
+scaleBand.domain([1, 2, 3, 4, 5]);
+scaleBand.range([0, 100]);
+scaleBand.padding(0.5);
+scaleBand.paddingOuter(1);
 
+let numList = Array(5).fill(0).map((item, index) => index + 1);
 
-//貌似d3 v4的pading规则变了
-let ordinal2 = d3.scalePoint();
-ordinal2.domain([1, 2, 3, 4, 5]);
-ordinal2.range([0, 100]);
-console.log(ordinal2(2));
-ordinal2.padding(1);
-for (let i = 1; i < 6; ++i) {
-    console.log(ordinal2(i));
-}
+numList.forEach(num => {
+    console.log(scaleBand(num));
+});
 
-ordinal2.rangeRound([0, 100]).padding(1);
-for (let i = 1; i < 6; ++i) {
-    console.log(ordinal2(i));
-}
+console.log("————");
+console.log(scaleBand.step());
+console.log(scaleBand.bandwidth());
+console.log(scaleBand.padding());
